5458f713a720fd8b622f1ed5a333201c93b05a34
Mark Grass <mgrass79@gmail.com>
2019-03-15 22:37:16 -0400

    - started working on an arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 #[macro_use] extern crate lazy_static;
 
-mod lib;
+mod haiku;
+mod arg_parser;
 
-use lib::{count_syllables};
+use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::string::String;
+
+use haiku::{count_syllables};
+use arg_parser::{ArgHandler, ArgParser};
 
 const ERR_READ_LINE : &str = "error reading string";
 const ERR_OPEN_FILE : &str = "error opening file";
 
 fn main() {
+    let parser = ArgParser::new(vec![
+        ArgHandler::new("-h", "print synopsis"),
+        ArgHandler::new("-f", "find haikus in file"),
+        ArgHandler::new("-u", "write haiku in cmd line"),
+    ]);
 
+    parser.interpret_args(&env::args().collect());
+    
     let mut buffer = open_file("test.txt");
     let mut line = String::new();
 
